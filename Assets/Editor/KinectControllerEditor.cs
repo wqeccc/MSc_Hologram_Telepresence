b033e0c69e113615de69249134e5d185b098f451
wqeccc <3369362481@qq.com>
2026-06-13 07:26:00 +0100

    Add Network & Editor

---
+++ Assets/Editor/KinectControllerEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(KinectController))]
+public class KinectControllerEditor : Editor
+{
+    string[] options;
+    int selected = 0;
+    
+    public void Awake()
+    {
+          options = new string[]
+         {
+             "WFOV_2x2Binned", "NFOV_2x2Binned", "WFOV_Unbinned","NFOV_Unbinned"
+         };
+    }
+    public override void OnInspectorGUI()
+    {
+        KinectController myTarget = (KinectController)target;
+        
+       switch (myTarget.depthMode)
+        {
+            case "WFOV_2x2Binned":
+                selected = 0;
+                break;
+            case "NFOV_2x2Binned":
+                selected = 1;
+                break;
+            case "WFOV_Unbinned":
+                selected = 2;
+                break;
+            case "NFOV_Unbinned":
+                selected = 3;
+                break;
+        }
+        selected = EditorGUILayout.Popup("Depth Mode", selected, options);
+        myTarget.depthMode = options[selected];
+
+    }
+}
