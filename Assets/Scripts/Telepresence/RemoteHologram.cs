5c227cadc5b6266c6b7439f61000f916ea753094
wqeccc <3369362481@qq.com>
2026-07-08 23:19:15 +0100

    Add plane detection

--- Assets/Scripts/Telepresence/RemoteHologram.cs
+++ Assets/Scripts/Telepresence/RemoteHologram.cs
@@ -7,6 +7,15 @@ using Microsoft.Azure.Kinect.Sensor;
 using Microsoft.Azure.Kinect.BodyTracking;
 #endif
 
+#if !UNITY_EDITOR && UNITY_ANDROID
+using UnityEngine.XR.OpenXR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using MagicLeap.OpenXR.Features.Planes;
+using MagicLeap.OpenXR.Subsystems;
+// using UnityEngine.InputSystem;
+#endif
+
 public class RemoteHologram : MonoBehaviour
 {
     private MetaDataNetworkSender _mdSender;
@@ -15,7 +24,7 @@ public class RemoteHologram : MonoBehaviour
     private GazeAlignment _gazeAlignment;
 
     #if UNITY_EDITOR || UNITY_STANDALONE_WIN
-    private KinectController _kinectController;
+        private KinectController _kinectController;
     #endif
 
     public bool enableGazeAlignment = true;
@@ -27,26 +36,59 @@ public class RemoteHologram : MonoBehaviour
     public Transform remoteSpeakerHologram; // P_b (S_b->a)
     public Pose localHologramAtRemote; // P_a (S_a->b)
 
-    void Start()
+    private bool initHologramPos = false;
+
+    #if !UNITY_EDITOR && UNITY_ANDROID
+        private ARPlaneManager planeManager;
+        private MagicLeapPlanesFeature planeFeature;
+        [SerializeField]
+        private uint maxResults = 100; // Maximum number of planes to return each query
+        [SerializeField]
+        private float minPlaneArea = 0.09f; // Minimum plane area to treat as a valid plane (m^2)
+        private bool permissionGranted = false;
+        // private MagicLeapController Controller => MagicLeapController.Instance;
+    #endif
+
+    IEnumerator Start()
     {
         #if UNITY_EDITOR || UNITY_STANDALONE_WIN
-        _kinectController = FindObjectOfType<KinectController>();
+            _kinectController = FindObjectOfType<KinectController>();
         #endif
 
         _pcReceiver = gameObject.AddComponent<PointCloudNetworkReceiver>();
         _mdReceiver = gameObject.AddComponent<MetaDataNetworkReceiver>();
         _mdSender = gameObject.AddComponent<MetaDataNetworkSender>();
         _gazeAlignment = gameObject.AddComponent<GazeAlignment>();
+
+        #if !UNITY_EDITOR && UNITY_ANDROID
+            // wait until the subsystem ready
+            yield return new WaitUntil(Utils.AreSubsystemsLoaded<XRPlaneSubsystem>);
+            planeManager = FindObjectOfType<ARPlaneManager>();
+            if (planeManager == null)
+            {
+                Debug.LogError("Failed to find ARPlaneManager in scene. Disabling Script");
+                enabled = false; // stop script
+            }
+            else
+            {
+                // disable planeManager until we have successfully requested required permissions
+                planeManager.enabled = false;
+            }
+
+            Permissions.RequestPermission(Permissions.SpatialMapping, OnPermissionGranted, OnPermissionDenied);
+        #endif
+
+        yield return null;
     }
 
     void Update()
     {
         #if UNITY_EDITOR || UNITY_STANDALONE_WIN
-        if (_kinectController == null || !_kinectController.kinectInitialized)
-        {
-            Debug.LogWarning("Kinect not Initialized");
-            return;
-        }
+            if (_kinectController == null || !_kinectController.kinectInitialized)
+            {
+                Debug.LogWarning("Kinect not Initialized");
+                return;
+            }
         #endif
 
         if (_mdReceiver != null)
@@ -116,7 +158,7 @@ public class RemoteHologram : MonoBehaviour
 
     void UpdateSenderData()
     {
-        if (_mdSender != null && localSpeaker != null)
+        if (_mdSender != null && localSpeaker != null && remoteSpeakerHologram != null)
         {
             _mdSender.localTransform_pos = localSpeaker.position;
             _mdSender.localTransform_rot = localSpeaker.rotation;
@@ -127,25 +169,133 @@ public class RemoteHologram : MonoBehaviour
 
     void CalculatePlacementPosition()
     {
-        if (localSpeaker == null && remoteSpeakerHologram == null)
+        if (localSpeaker == null || remoteSpeakerHologram == null || remoteSpeaker == null || remoteSpeaker.position.y < 0.1f)
         {
             return;
         }
 
-        Vector3 forwardVec = localSpeaker.forward;
-        forwardVec.y = 0;
-        forwardVec.Normalize();
+        // default position
+        if (!initHologramPos)
+        {
+            Vector3 forwardVec = localSpeaker.forward;
+            forwardVec.y = 0;
+            forwardVec.Normalize();
 
-        float distance = 1.5f; // comfortable social distances: 1.2m-3.7m
-        Vector3 targetPosXZ = localSpeaker.position + forwardVec * distance;
+            float distance = 1.5f; // comfortable social distances: 1.2m-3.7m
+            Vector3 targetPosXZ = localSpeaker.position + forwardVec * distance;
 
-        // TODO 
-        // y controller / plane / floor
-        float hologramPosY = 0f;
-        remoteSpeakerHologram.position = new Vector3(targetPosXZ.x, hologramPosY, targetPosXZ.z);
+            // assume the heights (HMDs) are the same
+            float defaultY = localSpeaker.position.y - remoteSpeaker.position.y;
+            Vector3 initialPos = new Vector3(targetPosXZ.x, defaultY, targetPosXZ.z);
+
+            #if !UNITY_EDITOR && UNITY_ANDROID
+                PlaneDetection(initialPos);
+            #else
+                remoteSpeakerHologram.position = initialPos;
+            #endif
+
+            initHologramPos = true;
+            Debug.Log("Inintialized hologram default position");
+            return;
+        }
 
         // TODO
+        // placing with controller/gesture
+        // PlaneDetection
+        // raycast hit??
+        // #if !UNITY_EDITOR && UNITY_ANDROID
+        // #endif
+
+        // TODO
+        // height scaling
         // default scale = (h_local - h_hologram_placement) / h_remote
         // remoteSpeakerHologram.localScale = new Vector3(targetPosXZ.x, hologramPosY, targetPosXZ.z);
     }
+
+    #if !UNITY_EDITOR && UNITY_ANDROID
+    void PlaneDetection(Vector3 pos)
+    {   
+        if (planeManager != null && !permissionGranted)
+        { 
+            remoteSpeakerHologram.position = pos;
+            return;
+        }
+
+        float finalY = pos.y; 
+        bool foundPlane = false;
+
+        foreach (var plane in planeManager.trackables)
+        {
+            // check if the pos is inside this plane
+            Vector2 pointInPlaneSpace = plane.transform.InverseTransformPoint(pos);
+                
+            if (Mathf.Abs(pointInPlaneSpace.x) <= plane.extents.x && Mathf.Abs(pointInPlaneSpace.y) <= plane.extents.y)
+            {
+                // ref: https://docs.unity3d.com/Packages/com.unity.xr.arfoundation@6.0/api/UnityEngine.XR.ARSubsystems.PlaneClassifications.html
+                switch (plane.classifications)
+                {
+                    case PlaneClassifications.Table:
+                    case PlaneClassifications.Floor:
+                    case PlaneClassifications.Seat:
+                    case PlaneClassifications.Couch:
+                    case PlaneClassifications.SeatOfAnyType:     
+                        finalY = plane.transform.position.y;
+                        foundPlane = true;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (foundPlane) break;
+            }
+        }
+
+        remoteSpeakerHologram.position = new Vector3(pos.x, finalY, pos.z);
+
+        StartCoroutine(StopPlanesScanning());
+    }
+
+    void StartPlanesScanning()
+    {
+        var newQuery = new MLXrPlaneSubsystem.PlanesQuery
+        {
+            Flags = planeManager.requestedDetectionMode.ToMLXrQueryFlags() | MLXrPlaneSubsystem.MLPlanesQueryFlags.SemanticAll,
+            BoundsCenter = Camera.main.transform.position,
+            BoundsRotation = Camera.main.transform.rotation,
+            BoundsExtents = Vector3.one * 20f,
+            MaxResults = maxResults,
+            MinPlaneArea = minPlaneArea
+        };
+
+        MLXrPlaneSubsystem.Query = newQuery;
+        planeManager.enabled = true;
+        Debug.Log("Start scanning planes");
+    }
+
+    IEnumerator StopPlanesScanning()
+    {
+        if (planeFeature != null && planeFeature.enabled)
+        {
+            planeFeature.InvalidateCurrentPlanes();
+        }
+        // Skip a frame for the changes to take effect and prefabs get removed.
+        yield return new WaitForEndOfFrame();
+        planeManager.enabled = false;
+        Debug.Log("Stop scanning planes");
+    }
+
+    void OnPermissionGranted(string permission)
+    {   
+        StartPlanesScanning();
+        permissionGranted = true;
+        planeFeature = OpenXRSettings.Instance.GetFeature<MagicLeapPlanesFeature>();
+    }
+
+    void OnPermissionDenied(string permission)
+    {
+        Debug.LogError($"Failed to create Planes Subsystem due to missing or denied {Permissions.SpatialMapping} permission. Please add to manifest. Disabling script.");
+        enabled = false;
+    }
+    #endif
 }
